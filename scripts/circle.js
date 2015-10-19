a852ca0308fd4393746a6d85c57fcf8e4eb121c3
omerkarj <omerkarj@gmail.com>
2015-10-19 23:55:15 +0300

    enemy circles

---
+++ scripts/circle.js
@@ -0,0 +1,46 @@
+
+var Circle = function (radius, speed, direction) {
+      this.radius = radius;
+      this.speed = speed;
+      this.direction = direction;
+      this.angle = 0;
+}
+
+Circle.prototype.rotate = function () {
+      this.angle += this.direction * this.speed;
+      this.draw();
+}
+Circle.prototype.draw = function () {
+      var x = window.centerX;
+      var y = window.centerY;
+      this.startAngle = convertToRadians(this.angle);
+      this.endAngle = startAngle + convertToRadians(12 * this.speed);
+
+      // clear last iteration
+      context.beginPath();
+      context.arc(x, y, this.radius, 0, 2 * Math.PI, false);
+      context.fillStyle = 'transparent';
+      context.fill();
+      context.lineWidth = 6;
+      context.strokeStyle = 'black';
+      context.stroke();
+            
+      // create the main arc
+      context.beginPath();
+      context.arc(x, y, this.radius, 0, 2 * Math.PI, false);
+      context.fillStyle = 'transparent';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = 'red';
+      context.stroke();
+
+      // create the opening in the arc
+      context.beginPath();
+      context.arc(x, y, this.radius, startAngle, endAngle, false);
+      context.fillStyle = 'transparent';
+      context.fill();
+      context.lineWidth = 6;
+      context.strokeStyle = 'black';
+      context.stroke();
+}
+
--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,11 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+var context = canvas.getContext("2d");
+canvas.width = 900;
+canvas.height = 900;
 document.body.appendChild(canvas);
-
-// Background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-bgImage.src = "images/background.png";
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
 
 // Game objects
 var hero = {
@@ -19,11 +13,25 @@ var hero = {
 	x: 0,
 	y: 0
 };
-var monster = {
-	x: 0,
-	y: 0
-};
-var monstersCaught = 0;
+
+var createCircles = function () {
+	window.circles = [];
+	for (var i = 0; i < 4; i++) {
+		var radius = 80 + i * 80;
+		var speed = 5 -  i;
+
+		window.circles.push(new Circle(radius, speed, i % 2 == 0 ? 1 : -1));	
+		window.circles[i].draw();
+		console.log("circle " + i + "drawn!");
+	}
+}
+var rotateCircles = function () {
+	window.circles.forEach( function (circle) {
+		circle.rotate();
+	});
+}
+
+
 
 // Handle keyboard controls
 var keysDown = {};
@@ -38,61 +46,22 @@ addEventListener("keyup", function (e) {
 
 // Reset the game when the player catches a monster
 var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
-
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+	window.circles = null;
+	createCircles();
 };
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
-
+	rotateCircles();
 	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
+	if (false) {
 		reset();
 	}
 };
 
 // Draw everything
 var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
-
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
-
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
-
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+	
 };
 
 // The main game loop
